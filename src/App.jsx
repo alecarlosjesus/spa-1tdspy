239631c31f5861e3e29908584842f185a24d3564
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2023-10-02 20:13:57 -0300

    Adcionados Cabeçalho,Conteudo,Rodape+Menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,2 +1,18 @@
+import Cabecalho from "./components/Cabecalho";
+import Conteudo from "./components/Conteudo";
+import Rodape from "./components/Rodape";
 
+export default function App(){
+
+  return(
+    <div>
+        <h1>APP</h1>
+
+        <Cabecalho/>
+        <Conteudo/>
+        <Rodape/>
+
+    </div>
+  )
+}
 
---
+++ src/components/Cabecalho.jsx
@@ -0,0 +1,18 @@
+
+export default function Cabecalho() {
+  return (
+    <>
+        
+     <header>
+        <nav>
+            <ul>
+                <li><a href="#">Item-1</a></li>
+                <li><a href="#">Item-2</a></li>
+                <li><a href="#">Item-3</a></li>
+            </ul>
+        </nav>
+     </header>
+
+    </>
+  )
+}
